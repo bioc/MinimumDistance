9dd7b06233204036afa6b69d48d2d0322e36bb51
Rob Scharpf <rscharpf@jhsph.edu>
2011-09-01 10:09:14 -0400

    remove duplicate import of class IntervalTree

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,14 @@
 THISPKG <- "MinimumDistance"
 .mdEnv <- new.env(parent=emptyenv())
 
-##.onLoad <- function(libname, pkgname) {
-##	require("methods")
-##}
+.onLoad <- function(libname, pkgname) {
+	require("methods")
+}
 
 .onAttach <- function(libname, pkgname) {
 	version <- packageDescription("MinimumDistance", field="Version")
-	message(getBar())
-	message("Welcome to MinimumDistance version ", version)
+	##message(getBar())
+	packageStartUpMessage(paste("Welcome to MinimumDistance version ", version))
 	options(prompt="R> ", continue=" ", width=70)##device=pdf
 	ocSamples(100)
 }
