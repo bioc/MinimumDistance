b8f6870a88c190cb9ba4905b45b4564e69822f3e
Rob Scharpf <rscharpf@jhsph.edu>
2012-02-09 10:25:29 -0500

    Use \Rclass{ff\_array} in MinimumDistance

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@ THISPKG <- "MinimumDistance"
 .mdEnv <- new.env(parent=emptyenv())
 
 .onAttach <- function(libname, pkgname) {
-	version <- packageDescription("MinimumDistance", field="Version")
+	version <- packageDescription("MinimumDistance", fields="Version")
 	packageStartupMessage(paste("Welcome to MinimumDistance version ", version))
 }
 
