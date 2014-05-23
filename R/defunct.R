717f03eed3b094de89531c701b71027bc4cf436f
Rob Scharpf <rscharpf@jhsph.edu>
2014-05-23 13:33:20 -0400

    add defunct.R

---
+++ R/defunct.R
@@ -0,0 +1,12 @@
+#' Defunct functions/classes/methods in the VanillaICE package
+#'
+#' The function, class, or data object you asked is defunct.
+#'
+#' @name coerce
+#' @aliases coerce,RangedDataCNV,GRanges-class
+#' @keywords internal
+#' @rdname Defunct
+#' @export
+setAs("RangedDataCNV", "GRanges", function(from, to){
+  .Defunct("RangedDataCNV class is defunct")
+})
